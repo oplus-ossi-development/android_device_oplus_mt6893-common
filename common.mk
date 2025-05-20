8080a2f6531baaad01e38f94413576e1d1477447
kailua <kailua@project2by2.jp>
2025-05-20 21:25:17 +0900

    mt6893-common: Add TARGET_BOARD_PLATFORM

--- common.mk
+++ common.mk
@@ -346,6 +346,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Platform
+TARGET_BOARD_PLATFORM := mt6893
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/publiclibraries/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
