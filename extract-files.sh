192c0bd3309dc051d9dc224d51c415c8d0275878
ManshuTyagi <himanshut567@gmail.com>
2025-05-21 14:42:31 +0000

    mt6893-common: Shim libsysenv with libbase_shim
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -89,7 +89,7 @@ function blob_fixup {
         vendor/lib/libcodec2_vndk-mtk.so)
             grep -q libshim_ui.so "$2" || "$PATCHELF" --add-needed libshim_ui.so "$2"
             ;;
-        vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron|vendor/lib*/libnvram.so|odm/bin/hw/vendor.oplus.hardware.charger@1.0-service)
+        vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron|vendor/lib*/libnvram.so|odm/bin/hw/vendor.oplus.hardware.charger@1.0-service|vendor/lib64/libsysenv.so)
              grep -q "libbase_shim.so" "${2}" || "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
              ;;
         vendor/lib64/hw/hwcomposer.mt6893.so)
