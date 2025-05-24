753910c6ea264ba454533377403f614c087b4a30
Himanshu Tyagi <himanshut567@gmail.com>
2025-05-24 20:57:40 +0000

    mt6893-common: Don't build AudioFX

--- common.mk
+++ common.mk
@@ -90,6 +90,8 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/aurisys/aurisys_config.xml:$(TARGET_COPY_OUT_ODM)/etc/audio/aurisys_config/aurisys_config.xml \
     $(COMMON_PATH)/configs/aurisys/aurisys_config_hifi3.xml:$(TARGET_COPY_OUT_ODM)/etc/audio/aurisys_config_hifi3/aurisys_config_hifi3.xml
 
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor:64 \
